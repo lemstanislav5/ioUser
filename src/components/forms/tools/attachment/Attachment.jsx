69e2349227f1ea8b7ec3ab8b5975d885756e791f
lemstanislav5 <lemstanislav5@gmail.com>
2023-02-28 21:58:00 +0300

    723098709723

--- src/components/forms/tools/attachment/Attachment.jsx
+++ src/components/forms/tools/attachment/Attachment.jsx
@@ -4,15 +4,19 @@ import { SvgImages } from '../../../images/SvgImages';
 
 export const Attachment = (props) => {
   const { color, upload, limitSizeFile } = props;
-  const [error, setError] = useState(false);
+  const [errorMb, setErrorMb] = useState(false);
+  const [errorType, setErrorType] = useState(false);
   const ref = useRef(null)
 
   const updateSize = (file) => {
     const mb = 1048576
-    if (file.size > mb * limitSizeFile) return setError(true)
-    //var filesExt = ['jpg','png','rar','zip','docx','pdf','rtf','doc'];
+    const type = file.type.replace('image/', '');
+    console.log(file.type.replace('image/', ''));
+    const filesExt = ['jpeg', 'jpg','png'];
+    console.log(file.size, mb * limitSizeFile, type, file.size > mb * limitSizeFile, filesExt.indexOf(type) === -1);
+    if (file.size > mb * limitSizeFile || filesExt.indexOf(type) === -1) return setErrorMb(true)
+    if (file.size > mb * limitSizeFile || filesExt.indexOf(type) === -1) return setErrorType(true)
     window._file = file;
-    console.log(file.size, file.type);
   }
   return(
     <>
@@ -20,7 +24,8 @@ export const Attachment = (props) => {
           console.log(ref)
           ref.current.click()
         }}>
-        {error && <div className={style.error}>Лимит файла в 5 МБ превышен</div>}
+        {errorMb && <div className={style.error}>Лимит файла в 5 МБ превышен</div>}
+        {errorType && <div className={style.error}>Допустимы орматы: 'jpeg', 'jpg','png'</div>}
         <SvgImages svg={'attachment'} fill={color} />
       </div>
       <input ref={ref} className={style.inputFile} type="file" onChange={event => updateSize(event.target.files[0])}/>
