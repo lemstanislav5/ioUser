ff3eab9d8905473f06462d1f651c41c12a21d328
lemstanislav5 <lemstanislav5@gmail.com>
2023-03-05 19:58:18 +0300

    0972309172309712093

--- src/components/forms/tools/record/Record.jsx
+++ src/components/forms/tools/record/Record.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './Record.module.css';
 import { SvgImages } from '../../../images/SvgImages';
+const stream =  navigator.mediaDevices.getUserMedia({audio: true});
+let mediaRecorder = new MediaRecorder(stream)
 
 export const Record = (props) => {
   const { color } = props;
-  //https://habr.com/ru/post/484196/
+  let [record, setRecord] = useState(false);
+  let [voice, setVoice] = useState([]);
+  // let [mediaRecorder, setMediaRecorder] = useState(null);
+  
+  
+  
+  useEffect(async ()=> {
+    
+    mediaRecorder.addEventListener("dataavailable",function(event) {
+      setVoice([...voice, event.data]);
+    });
+  }, [])
+
+  const startRecording = () => {
+    mediaRecorder.start();
+  }
+
+  const stopRecording = () => {
+    mediaRecorder.stop();
+    const voiceBlob = new Blob(voice, { type: 'audio/wav' });
+    console.log(voiceBlob)
+  }
+
+//https://stackoverflow.com/questions/50431236/use-getusermedia-media-devices-in-reactjs-to-record-audio
+  useEffect(() => {
+    if(record) return startRecording();
+    stopRecording();
+  }, [record, voice]);
+
   return(
     <>
       <div className={style.record} onClick={() => {}}>
-        <SvgImages svg={'record'} fill={color} />
+        <SvgImages svg={'record'} fill={color} onClick={() => setRecord(record? false: true)}/>
       </div>
     </>
   )
