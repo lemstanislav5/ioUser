1be20682b86bf41393701fbf12dd880a402800f0
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-15 18:38:38 +0300

    43241234

--- src/components/forms/messegesBox/MessegesBox.jsx
+++ src/components/forms/messegesBox/MessegesBox.jsx
@@ -5,12 +5,12 @@ import style from './MessegesBox.module.css';
 import MyImage from './image/MyImage';
 import FileAvailabilityCheck from '../../hocs/FileAvailabilityCheck';
 import Document from './document/Document';
+
 export const MessegesBox = (props) => {
   const { messeges, colors, SvgImages } = props;
 
   return(
     messeges.map((item, i) => {
-      console.log(item.date)
       return (
         <div className={style.msgbox} key={'msg' + i}>
           <div className={style[item.type]} key={i}  style={{'backgroundColor': colors[item.type]}}>
